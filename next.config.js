3d0e07af7db72438b736f9a95c0e2ef887ebf278
Christopher Vu <christophervu03@gmail.com>
2024-03-13 16:44:38 -0500

    readding onnx runtime

--- next.config.js
+++ next.config.js
@@ -5,9 +5,9 @@ const nextConfig = {
   output: 'standalone', // Feel free to modify/remove this option
   
   // Indicate that these packages should not be bundled by webpack
-  // experimental: {
-  //     serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
-  // },
+  experimental: {
+      serverComponentsExternalPackages: ['sharp', 'onnxruntime-node'],
+  },
 };
 
 module.exports = nextConfig;
